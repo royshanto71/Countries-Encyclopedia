d933972b32b55814e285b0dd94f738895ad3fafa
Roy Shanto <122017004+royshanto71@users.noreply.github.com>
2025-09-23 04:43:09 +0600

    update App.jsx and Countries.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import React, { Suspense } from "react";
 import Countries from "./Components/Countries/Countries";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all"
+).then((response) => response.json());
 
 function App() {
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Countries countriesPromise={countriesPromise} />
+      </Suspense>
     </>
   );
 }
--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { use } from "react";
 
-function Countries() {
+function Countries({ countriesPromise }) {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  console.log(countries);
+
   return (
-    <div>Countries</div>
-  )
+    <div>
+      <h2>Countries List</h2>
+      {countries.map((country) => (
+        <ul>
+          <li>
+            {country.name.common}
+            <div>
+              <img src={country.flags.flags.png} />
+            </div>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 }
 
-export default Countries
+export default Countries;
